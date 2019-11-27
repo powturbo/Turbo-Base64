ccb3027fe54e817d47a0389c5d07aa274528a124
x <powturbo@gmail.com>
2019-11-27 22:43:53 +0100

    Benchmark App

--- tb64app.c
+++ tb64app.c
@@ -72,7 +72,7 @@ void bench(unsigned char *in, unsigned n, unsigned char *out, unsigned char *cpy
   switch(id) {
     case 1:                    TMBENCH("",l=tb64senc(   in, n, out),n); pr(l,n); TMBENCH2("tb64s",      tb64sdec(out, l, cpy), n);     break;
     case 2:                    TMBENCH("",l=tb64xenc(   in, n, out),n); pr(l,n); TMBENCH2("tb64x",      tb64xdec( out, l, cpy), n);    break;
-      #if defined(__i386__) || defined(__x86_64__) || defined(__ARM_NEON) || defined(_ARCH_PWR9)
+      #if defined(__i386__) || defined(__x86_64__) || defined(__ARM_NEON) || defined(__powerpc64__)
     case 3:                    TMBENCH("",l=tb64enc(    in, n, out),n); pr(l,n); TMBENCH2("tb64(auto)", tb64dec(out, l, cpy), n);      break;
     case 4:if(cpuini(0)>=33) { TMBENCH("",l=tb64sseenc( in, n, out),n); pr(l,n); TMBENCH2("tb64sse",    tb64ssedec(out, l, cpy), n); } break;
       #else

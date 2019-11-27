f984ea1ad42b25b06c41574e4b92e6e7b40264db
x <powturbo@gmail.com>
2019-11-27 12:47:21 +0100

    Benchmark App

--- tb64app.c
+++ tb64app.c
@@ -30,7 +30,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
     - twitter  : https://twitter.com/powturbo
     - email    : powturbo [_AT_] gmail [_DOT_] com
 **/
-// Turbase64: TB64app.c - Benchmark app
+// Turbo Base64: TB64app.c - Benchmark app
 
 #include <string.h> 
 #include <stdio.h>

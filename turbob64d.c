20af5f832583a08e38e2688445589c636e2c81e7
x <powturbo@gmail.com>
2019-12-03 14:14:38 +0100

    Turbo Base64: Decode scalar

--- turbob64d.c
+++ turbob64d.c
@@ -356,10 +356,11 @@ unsigned tb64xdec(const unsigned char *in, unsigned inlen, unsigned char *out) {
       if(  ip[2]=='=') { l = 2; 
         if(ip[1]=='=')   l = 1; 
       }
-    }												 printf("l=%d ", l);
+    }												 
   unsigned char *up=(unsigned char *)&u;  
   switch(l) {
-    case 4: u = lut0[ip[0]] | lut1[ip[1]] | lut2[ip[2]] | lut3[ip[3]]; *op++ = up[0]; *op++ = up[1]; *op++ = up[2]; cu |= u; break; // 4->3 bytes
+    case 4: u = ctou32(ip); u = LU32(u); //lut0[ip[0]] | lut1[ip[1]] | lut2[ip[2]] | lut3[ip[3]]; 
+                 *op++ = up[0]; *op++ = up[1]; *op++ = up[2]; cu |= u; break; // 4->3 bytes
     case 3: u = lut0[ip[0]] | lut1[ip[1]] | lut2[ip[2]];               *op++ = up[0]; *op++ = up[1];                cu |= u; break; // 3->2 bytes
     case 2: u = lut0[ip[0]] | lut1[ip[1]];                             *op++ = up[0];                               cu |= u; break; // 2->1 byte
     case 1: u = lut0[ip[0]];                                           *op++ = up[0];                               cu |= u; break; // 1->1 byte

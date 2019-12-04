8b8513d66dfacf30ec6752d73321dde48659c637
x <powturbo@gmail.com>
2019-12-04 22:24:57 +0100

    Turbo Base64: Encode scalar

--- turbob64c.c
+++ turbob64c.c
@@ -349,8 +349,9 @@ static const unsigned short lut2[1<<12] = {
                    lut2[ _u_ >> 20])
                              
 #define EI32(_i_) {\
-  unsigned _u = BSWAP32(ux); ux = ctou32(ip+6+_i_*6  );\
-  unsigned _v = BSWAP32(vx); vx = ctou32(ip+6+_i_*6+3);\
+  unsigned _u = ux, _v = vx;\
+  ux = BSWAP32(ctou32(ip+6+_i_*6  ));  \
+  vx = BSWAP32(ctou32(ip+6+_i_*6+3));\
   _u = EU32(_u); _v = EU32(_v); \
   ctou32(op+_i_*8) = _u; ctou32(op+_i_*8+4) = _v; \
 }                  
@@ -362,9 +363,9 @@ unsigned tb64xenc(const unsigned char *in, unsigned inlen, unsigned char *out) {
          unsigned      outlen = TURBOB64LEN(inlen);
   
   if(outlen >= 128+4) {
-    unsigned ux = ctou32(ip  ),
-             vx = ctou32(ip+3);
-    for(; op <= out+(outlen-(128+4)); op += 128, ip += (128/4)*3) { // unrolling 96 bytes
+    unsigned ux = BSWAP32(ctou32(ip  )),
+             vx = BSWAP32(ctou32(ip+3));
+    for(; op <= out+(outlen-(128+4)); op += 128, ip += (128/4)*3) { // unrolling 96->128 bytes
       EI32(0); EI32(1); EI32( 2); EI32( 3); EI32( 4); EI32( 5); EI32( 6); EI32( 7);      
       EI32(8); EI32(9); EI32(10); EI32(11); EI32(12); EI32(13); EI32(14); EI32(15);      PREFETCH(ip,256, 0);
     }

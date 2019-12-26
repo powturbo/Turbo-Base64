d1fd783aeb2eeed3cdbd1366af407562a9c81e5f
x <powturbo@gmail.com>
2019-12-26 19:40:17 +0100

    Turbo Base64: c/c++ header

--- turbob64.h
+++ turbob64.h
@@ -40,13 +40,13 @@ extern "C" {
 //---------------------- base64 API functions ----------------------------------
 // Base64 output length after encoding 
 #define TB64ENCLEN(_n_) ((_n_ + 2)/3 * 4)
-unsigned tb64memcpy(const unsigned char *in, unsigned inlen, unsigned char *out);
+size_t tb64memcpy(const unsigned char *in, size_t inlen, unsigned char *out);
 
 // return the base64 buffer length after encoding
-unsigned tb64enclen(unsigned inlen);
+size_t tb64enclen(size_t inlen);
 
 // return the original (after decoding) length for a given base64 encoded buffer
-unsigned tb64declen(const unsigned char *in, unsigned inlen);
+size_t tb64declen(const unsigned char *in, size_t inlen);
 
 // Encode binary input 'in' buffer into base64 string 'out' 
 // with automatic cpu detection for avx2/sse4.1/scalar 
@@ -56,7 +56,7 @@ unsigned tb64declen(const unsigned char *in, unsigned inlen);
 // return value: Length of output buffer
 // Remark      : byte 'zero' is not written to end of output stream
 //               Caller must add 0 (out[outlen] = 0) for a null terminated string
-unsigned tb64enc(const unsigned char *in, unsigned inlen, unsigned char *out);
+size_t tb64enc(const unsigned char *in, size_t inlen, unsigned char *out);
 
 // Decode base64 input 'in' buffer into binary buffer 'out' 
 // in          : input buffer to decode
@@ -64,31 +64,31 @@ unsigned tb64enc(const unsigned char *in, unsigned inlen, unsigned char *out);
 // out         : output buffer
 // return value: >0 output buffer length
 //                0 Error (invalid base64 input or input length = 0)
-unsigned tb64dec(const unsigned char *in, unsigned inlen, unsigned char *out);
+size_t tb64dec(const unsigned char *in, size_t inlen, unsigned char *out);
 
 //---------------------- base64 Internal functions ------------------------------
 // Space efficient scalar but (slower) version
-unsigned tb64senc(   const unsigned char *in, unsigned inlen, unsigned char *out);
-unsigned tb64sdec(   const unsigned char *in, unsigned inlen, unsigned char *out);
+size_t tb64senc(   const unsigned char *in, size_t inlen, unsigned char *out);
+size_t tb64sdec(   const unsigned char *in, size_t inlen, unsigned char *out);
 
 // Fast scalar base64
-unsigned tb64xenc(   const unsigned char *in, unsigned inlen, unsigned char *out);
-unsigned tb64xdec(   const unsigned char *in, unsigned inlen, unsigned char *out);
+size_t tb64xenc(   const unsigned char *in, size_t inlen, unsigned char *out);
+size_t tb64xdec(   const unsigned char *in, size_t inlen, unsigned char *out);
 
 // ssse3 base64 
-unsigned tb64sseenc( const unsigned char *in, unsigned inlen, unsigned char *out);
-unsigned tb64ssedec( const unsigned char *in, unsigned inlen, unsigned char *out);
+size_t tb64sseenc( const unsigned char *in, size_t inlen, unsigned char *out);
+size_t tb64ssedec( const unsigned char *in, size_t inlen, unsigned char *out);
 
 // avx base64 
-unsigned tb64avxenc( const unsigned char *in, unsigned inlen, unsigned char *out);
-unsigned tb64avxdec( const unsigned char *in, unsigned inlen, unsigned char *out);
+size_t tb64avxenc( const unsigned char *in, size_t inlen, unsigned char *out);
+size_t tb64avxdec( const unsigned char *in, size_t inlen, unsigned char *out);
 
 // avx2 base 64
-unsigned tb64avx2enc(const unsigned char *in, unsigned inlen, unsigned char *out);
-unsigned tb64avx2dec(const unsigned char *in, unsigned inlen, unsigned char *out);
+size_t tb64avx2enc(const unsigned char *in, size_t inlen, unsigned char *out);
+size_t tb64avx2dec(const unsigned char *in, size_t inlen, unsigned char *out);
 
-unsigned tb64avx512enc(const unsigned char *in, unsigned inlen, unsigned char *out);
-unsigned tb64avx512dec(const unsigned char *in, unsigned inlen, unsigned char *out);
+size_t tb64avx512enc(const unsigned char *in, size_t inlen, unsigned char *out);
+size_t tb64avx512dec(const unsigned char *in, size_t inlen, unsigned char *out);
 
 void tb64ini(int id);  // detect cpu && set the default run time functions for tb64enc/tb64dec
  
@@ -100,6 +100,11 @@ unsigned cpuini(unsigned cpuisa);
 // convert simd set to string "sse3", "ssse3", "sse4.1", "avx", "avx2", "neon",... 
 // Ex.: printf("current cpu set=%s\n", cpustr(cpuini(0)) ); 
 char *cpustr(unsigned cpuisa); 
+
+  #ifdef TB64_IN  // internal functions
+size_t _tb64xenc(   const unsigned char *in, size_t inlen, unsigned char *out);
+size_t _tb64xdec(   const unsigned char *in, size_t inlen, unsigned char *out);
+  #endif
 
 #ifdef __cplusplus
 }

9393a4a4d0e1baf46e023b1586cf38915e1c0a67
x <powturbo@gmail.com>
2023-04-03 12:53:28 +0200

    Turbo Base64: Encode/Decode avx2

--- turbob64v256.c
+++ turbob64v256.c
@@ -30,51 +30,49 @@
 
 #include <immintrin.h>
 
-#define UA_MEMCPY
-#include "conf.h"
-#include "turbob64.h"
 #include "turbob64_.h"
+#include "turbob64.h"
 
 //--------------------- Decode ----------------------------------------------------------------------
-#define MM256_PACK8TO6(v,cpv) {\
+#define BITPACK256V8_6(v,cpv) {\
   const __m256i merge_ab_and_bc = _mm256_maddubs_epi16(v,            _mm256_set1_epi32(0x01400140));\
                               v = _mm256_madd_epi16(merge_ab_and_bc, _mm256_set1_epi32(0x00011000));\
                               v = _mm256_shuffle_epi8(v, cpv);\
 }
 
-#define MM256_MAP8TO6(iv, shifted, delta_asso, delta_values, ov) { /*map 8-bits ascii to 6-bits bin*/\
+#define BITMAP256V8_6(iv, shifted, delta_asso, delta_values, ov) { /*map 8-bits ascii to 6-bits bin*/\
                 shifted    = _mm256_srli_epi32(iv, 3);\
   const __m256i delta_hash = _mm256_avg_epu8(_mm256_shuffle_epi8(delta_asso, iv), shifted);\
                         ov = _mm256_add_epi8(_mm256_shuffle_epi8(delta_values, delta_hash), iv);\
 }
 
-#define MM256_B64CHK(iv, shifted, check_asso, check_values, vx) {\
+#define B64CHK256(iv, shifted, check_asso, check_values, vx) {\
   const __m256i check_hash = _mm256_avg_epu8( _mm256_shuffle_epi8(check_asso, iv), shifted);\
   const __m256i        chk = _mm256_adds_epi8(_mm256_shuffle_epi8(check_values, check_hash), iv);\
                         vx = _mm256_or_si256(vx, chk);\
 }
 
 #define DS256(_i_) {\
-  __m256i iv0 = _mm256_loadu_si256((__m256i *)(ip+64+_i_*128+0)),    \
+  __m256i iv0 = _mm256_loadu_si256((__m256i *)(ip+64+_i_*128+0)), \
           iv1 = _mm256_loadu_si256((__m256i *)(ip+64+_i_*128+32));\
-\
-  __m256i ou0,shiftedu0; MM256_MAP8TO6(iu0, shiftedu0, delta_asso, delta_values, ou0); MM256_PACK8TO6(ou0, cpv);\
-  __m256i ou1,shiftedu1; MM256_MAP8TO6(iu1, shiftedu1, delta_asso, delta_values, ou1); MM256_PACK8TO6(ou1, cpv);\
-      \
-  CHECK0(MM256_B64CHK(iu0,shiftedu0, check_asso, check_values, vx));\
+                                                                  \
+  __m256i ou0,shiftedu0; BITMAP256V8_6(iu0, shiftedu0, delta_asso, delta_values, ou0); BITPACK256V8_6(ou0, cpv);\
+  __m256i ou1,shiftedu1; BITMAP256V8_6(iu1, shiftedu1, delta_asso, delta_values, ou1); BITPACK256V8_6(ou1, cpv);\
+                                                                  \
+  CHECK0(B64CHK256(iu0,shiftedu0, check_asso, check_values, vx)); \
           iu0 = _mm256_loadu_si256((__m256i *)(ip+64+_i_*128+64));\
-  CHECK1(MM256_B64CHK(iu1,shiftedu1, check_asso, check_values, vx));\
+  CHECK1(B64CHK256(iu1,shiftedu1, check_asso, check_values, vx)); \
           iu1 = _mm256_loadu_si256((__m256i *)(ip+64+_i_*128+96));\
   _mm_storeu_si128((__m128i*)(op+_i_*96   ), _mm256_castsi256_si128(  ou0   ));\
   _mm_storeu_si128((__m128i*)(op+_i_*96+12), _mm256_extracti128_si256(ou0, 1));\
   _mm_storeu_si128((__m128i*)(op+_i_*96+24), _mm256_castsi256_si128(  ou1   ));\
   _mm_storeu_si128((__m128i*)(op+_i_*96+36), _mm256_extracti128_si256(ou1, 1));\
-  \
-  __m256i ov0,shiftedv0; MM256_MAP8TO6(iv0, shiftedv0, delta_asso, delta_values, ov0); MM256_PACK8TO6(ov0, cpv);\
-  __m256i ov1,shiftedv1; MM256_MAP8TO6(iv1, shiftedv1, delta_asso, delta_values, ov1); MM256_PACK8TO6(ov1, cpv); \
-      \
-  CHECK1(MM256_B64CHK(iv0, shiftedv0, check_asso, check_values, vx));\
-  CHECK1(MM256_B64CHK(iv1, shiftedv1, check_asso, check_values, vx));\
+                                                                               \
+  __m256i ov0,shiftedv0; BITMAP256V8_6(iv0, shiftedv0, delta_asso, delta_values, ov0); BITPACK256V8_6(ov0, cpv);\
+  __m256i ov1,shiftedv1; BITMAP256V8_6(iv1, shiftedv1, delta_asso, delta_values, ov1); BITPACK256V8_6(ov1, cpv);\
+                                                                  \
+  CHECK1(B64CHK256(iv0, shiftedv0, check_asso, check_values, vx));\
+  CHECK1(B64CHK256(iv1, shiftedv1, check_asso, check_values, vx));\
   _mm_storeu_si128((__m128i*)(op+_i_*96+48), _mm256_castsi256_si128(  ov0   ));\
   _mm_storeu_si128((__m128i*)(op+_i_*96+60), _mm256_extracti128_si256(ov0, 1));\
   _mm_storeu_si128((__m128i*)(op+_i_*96+72), _mm256_castsi256_si128(  ov1   ));\
@@ -114,22 +112,22 @@ size_t tb64v256dec(const unsigned char *__restrict in, size_t inlen, unsigned ch
     }	
     for(; ip < (in+inlen)-32-4; ip += 32, op += 32*3/4) {
       __m256i iv = _mm256_loadu_si256((__m256i *)ip);      
-      __m256i ov,shifted0; MM256_MAP8TO6(iv, shifted0, delta_asso, delta_values, ov); MM256_PACK8TO6(ov, cpv);
+      __m256i ov,shifted0; BITMAP256V8_6(iv, shifted0, delta_asso, delta_values, ov); BITPACK256V8_6(ov, cpv);
       
       _mm_storeu_si128((__m128i*) op,       _mm256_castsi256_si128(ov));
       _mm_storeu_si128((__m128i*)(op + 12), _mm256_extracti128_si256(ov, 1));                          
      
-      CHECK1(MM256_B64CHK(iv, shifted0, check_asso, check_values, vx));
+      CHECK1(B64CHK256(iv, shifted0, check_asso, check_values, vx));
     }
     _vx = _mm_or_si128(_mm256_extracti128_si256(vx, 1), _mm256_castsi256_si128(vx));
   }
   for(;ip < (in+inlen)-16-4;ip += 16, op += 16*3/4) {
     __m128i iv0 = _mm_loadu_si128((__m128i *) ip);
-    __m128i ov0, shifted0; MM_MAP8TO6( iv0, shifted0,_mm256_castsi256_si128(delta_asso),_mm256_castsi256_si128(delta_values), ov0); 
-	                       MM_PACK8TO6(ov0, _mm256_castsi256_si128(cpv));
+    __m128i ov0, shifted0; BITMAP128V8_6( iv0, shifted0,_mm256_castsi256_si128(delta_asso),_mm256_castsi256_si128(delta_values), ov0); 
+	                       BITPACK128V8_6(ov0, _mm256_castsi256_si128(cpv));
     _mm_storeu_si128((__m128i*) op, ov0);  
     
-    CHECK0(MM_B64CHK(iv0, shifted0, _mm256_castsi256_si128(check_asso), _mm256_castsi256_si128(check_values), _vx));
+    CHECK0(B64CHK128(iv0, shifted0, _mm256_castsi256_si128(check_asso), _mm256_castsi256_si128(check_values), _vx));
   }
   unsigned cx = _mm_movemask_epi8(_vx);
   size_t rc = 0, r = inlen-(ip-in); 
@@ -146,8 +144,8 @@ size_t tb64v256dec(const unsigned char *__restrict in, size_t inlen, unsigned ch
   __m256i v1 = _mm256_castsi128_si256(    _mm_loadu_si128((__m128i *)(ip+48+_i_*96+24))  );\
           v1 = _mm256_inserti128_si256(v1,_mm_loadu_si128((__m128i *)(ip+48+_i_*96+36)),1);\
                                                                                            \
-  u0 = _mm256_shuffle_epi8(u0, shuf); u0 = mm256_unpack6to8(u0); u0 = mm256_map6to8(u0);\
-  u1 = _mm256_shuffle_epi8(u1, shuf); u1 = mm256_unpack6to8(u1); u1 = mm256_map6to8(u1);\
+  u0 = _mm256_shuffle_epi8(u0, shuf); u0 = bitunpack256v8_6(u0); u0 = bitmap256v8_6(u0);\
+  u1 = _mm256_shuffle_epi8(u1, shuf); u1 = bitunpack256v8_6(u1); u1 = bitmap256v8_6(u1);\
        _mm256_storeu_si256((__m256i*)(op+_i_*128),    u0);                              \
        _mm256_storeu_si256((__m256i*)(op+_i_*128+32), u1);                              \
 		                                                                                   \
@@ -156,8 +154,8 @@ size_t tb64v256dec(const unsigned char *__restrict in, size_t inlen, unsigned ch
           u1 = _mm256_castsi128_si256(    _mm_loadu_si128((__m128i *)(ip+48+_i_*96+72))  );\
           u1 = _mm256_inserti128_si256(u1,_mm_loadu_si128((__m128i *)(ip+48+_i_*96+84)),1); \
                                                                                            \
-  v0 = _mm256_shuffle_epi8(v0, shuf); v0 = mm256_unpack6to8(v0); v0 = mm256_map6to8(v0);\
-  v1 = _mm256_shuffle_epi8(v1, shuf); v1 = mm256_unpack6to8(v1); v1 = mm256_map6to8(v1); \
+  v0 = _mm256_shuffle_epi8(v0, shuf); v0 = bitunpack256v8_6(v0); v0 = bitmap256v8_6(v0);\
+  v1 = _mm256_shuffle_epi8(v1, shuf); v1 = bitunpack256v8_6(v1); v1 = bitmap256v8_6(v1); \
        _mm256_storeu_si256((__m256i*)(op+_i_*128+64), v0);\
        _mm256_storeu_si256((__m256i*)(op+_i_*128+96), v1);\
 }
@@ -188,15 +186,15 @@ size_t tb64v256enc(const unsigned char *__restrict in, size_t inlen, unsigned ch
   for(; op < out+outlen-32; op += 32, ip += 32*3/4) {
     __m256i v = _mm256_castsi128_si256(   _mm_loadu_si128((__m128i *) ip    )  );      
             v = _mm256_inserti128_si256(v,_mm_loadu_si128((__m128i *)(ip+12)),1);   
-            v = _mm256_shuffle_epi8(v, shuf); v = mm256_unpack6to8(v); v = mm256_map6to8(v);                                                                                                           
+            v = _mm256_shuffle_epi8(v, shuf); v = bitunpack256v8_6(v); v = bitmap256v8_6(v);                                                                                                           
                 _mm256_storeu_si256((__m256i*) op, v);                                                 
   }
   /*for(; op <= (out+outlen)-16; op += 16, ip += (16/4)*3) {
     const __m128i    shuf = _mm_set_epi8(10,11, 9,10, 7, 8, 6, 7, 4,   5, 3, 4, 1, 2, 0, 1);
     __m128i v0 = _mm_loadu_si128((__m128i*)ip);
             v0 = _mm_shuffle_epi8(v0, shuf);
-            v0 = mm_unpack6to8(v0);
-            v0 = mm_map6to8(v0);
+            v0 = bitunpack128v8_6(v0);
+            v0 = bitmap128v8_6(v0);
     _mm_storeu_si128((__m128i*) op, v0);                                          
   }*/
   EXTAIL();
@@ -226,22 +224,22 @@ size_t _tb64v256dec(const unsigned char *in, size_t inlen, unsigned char *out) {
     __m256i vx = _mm256_setzero_si256();
     for(ip = in, op = out; ip < (in+inlen)-32; ip += 32, op += (32/4)*3) {
       __m256i          iv0 = _mm256_loadu_si256((__m256i *)ip);
-      __m256i ov0,shifted0; MM256_MAP8TO6(iv0, shifted0, delta_asso, delta_values, ov0); MM256_PACK8TO6(ov0, cpv);
+      __m256i ov0,shifted0; BITMAP256V8_6(iv0, shifted0, delta_asso, delta_values, ov0); BITPACK256V8_6(ov0, cpv);
       
       _mm_storeu_si128((__m128i*) op,       _mm256_castsi256_si128(ov0));
       _mm_storeu_si128((__m128i*)(op + 12), _mm256_extracti128_si256(ov0, 1));                          
-      CHECK0(MM256_B64CHK(iv0, shifted0, check_asso, check_values, vx));
+      CHECK0(B64CHK256(iv0, shifted0, check_asso, check_values, vx));
     }
 
     unsigned cx;
     if(ip < (in+inlen)-16) {
       __m128i iv0 = _mm_loadu_si128((__m128i *) ip);
       __m128i _vx = _mm_or_si128(_mm256_extracti128_si256(vx, 1), _mm256_castsi256_si128(vx));
-      __m128i ov0, shifted0; MM_MAP8TO6( iv0, shifted0,_mm256_castsi256_si128(delta_asso),_mm256_castsi256_si128(delta_values), ov0); 
-	                         MM_PACK8TO6(ov0, _mm256_castsi256_si128(cpv));
+      __m128i ov0, shifted0; BITMAP128V8_6( iv0, shifted0,_mm256_castsi256_si128(delta_asso),_mm256_castsi256_si128(delta_values), ov0); 
+	                         BITPACK128V8_6(ov0, _mm256_castsi256_si128(cpv));
       _mm_storeu_si128((__m128i*) op, ov0);  
       ip += 16; op += (16/4)*3;
-      CHECK0(MM_B64CHK(iv0, shifted0, _mm256_castsi256_si128(check_asso), _mm256_castsi256_si128(check_values), _vx));
+      CHECK0(B64CHK128(iv0, shifted0, _mm256_castsi256_si128(check_asso), _mm256_castsi256_si128(check_values), _vx));
       cx = _mm_movemask_epi8(_vx);
     } else
       cx = _mm256_movemask_epi8(vx);
@@ -264,7 +262,7 @@ size_t _tb64v256enc(const unsigned char* in, size_t inlen, unsigned char *out) {
     for(; op <= (out+outlen)-32; op += 32, ip += (32/4)*3) {
       __m256i v0 = _mm256_castsi128_si256(    _mm_loadu_si128((__m128i *) ip));      
               v0 = _mm256_inserti128_si256(v0,_mm_loadu_si128((__m128i *)(ip+12)),1);   
-      v0 = _mm256_shuffle_epi8(v0, shuf); v0 = mm256_unpack6to8(v0); v0 = mm256_map6to8(v0);                                                                                                           
+      v0 = _mm256_shuffle_epi8(v0, shuf); v0 = bitunpack256v8_6(v0); v0 = bitmap256v8_6(v0);                                                                                                           
       _mm256_storeu_si256((__m256i*) op,     v0);                                            
     }
   }
@@ -273,8 +271,8 @@ size_t _tb64v256enc(const unsigned char* in, size_t inlen, unsigned char *out) {
 
     __m128i v0 = _mm_loadu_si128((__m128i*)ip);
             v0 = _mm_shuffle_epi8(v0, shuf);
-            v0 = mm_unpack6to8(v0);
-            v0 = mm_map6to8(v0);
+            v0 = bitunpack128v8_6(v0);
+            v0 = bitmap128v8_6(v0);
     _mm_storeu_si128((__m128i*) op, v0);                                          
     op += 16; ip += (16/4)*3;
   }

726389db482b903675abeb2b2b0b03769cae9407
x <powturbo@gmail.com>
2023-04-07 00:55:21 +0200

    Turbo Base64: include

--- turbob64_.h
+++ turbob64_.h
@@ -59,7 +59,7 @@ extern unsigned char tb64lutse[];
                    tb64lutse[(_u_>>26) & 0x3f])
 
 #define ETAIL()\
-  unsigned _l = (in+inlen) - ip;\
+  unsigned _l = (in+inlen) - ip;   AS(ip <= in+inlen, "ETAIL:Fatal %d\n", ip - (in+inlen));\
        if(_l == 3) { unsigned _u = ip[0]<<24 | ip[1]<<16 | ip[2]<<8; stou32(op, SU32(_u)); op+=4; }\
   else if(_l == 2) { op[0] = tb64lutse[(ip[0]>>2)&0x3f]; op[1] = tb64lutse[(ip[0] & 0x3) << 4 | (ip[1] & 0xf0) >> 4]; op[2] = tb64lutse[(ip[1] & 0xf) << 2]; op[3] = '='; op+=4; }\
   else if(_l)      { op[0] = tb64lutse[(ip[0]>>2)&0x3f]; op[1] = tb64lutse[(ip[0] & 0x3) << 4],                       op[2] = '=';                           op[3] = '='; op+=4; }
@@ -68,7 +68,7 @@ extern const unsigned short tb64lute[];
 #define XU32(_u_) (tb64lute[(_u_ >>  8) & 0xfff] << 16 |\
                    tb64lute[ _u_ >> 20])
 
-#define EXTAIL() for(; op < (out+outlen)-4; op += 4, ip += 3) { unsigned _u = BSWAP32(ctou32(ip)); stou32(op, XU32(_u)); } ETAIL()
+#define EXTAIL(_n_) for(; op < out_-4; op += 4, ip += 3) { unsigned _u = BSWAP32(ctou32(ip)); stou32(op, XU32(_u)); } ETAIL()
 
 //------- Decode: scalar helper macros & functions ----------------------------------------------------------
 extern const unsigned tb64lutd0[];
@@ -96,15 +96,15 @@ extern const unsigned tb64lutd3[];
 }
 
 static ALWAYS_INLINE size_t _tb64xd(const unsigned char *in, size_t inlen, unsigned char *out) { 
-  const unsigned char *ip = in;
-        unsigned char *op = out;  
+  const unsigned char *ip = in, *in_ = in+inlen;
+        unsigned char *op = out;
     #ifdef B64CHECK
   unsigned       cu = 0;
-  for(; ip < (in+inlen)-4; ip += 4, op += 3) { unsigned u = ctou32(ip); u = DU32(u); stou32(op, u); cu |= u; }
+  for(; ip < in_-4; ip += 4, op += 3) { unsigned u = ctou32(ip); u = DU32(u); stou32(op, u); cu |= u; }
   DXTAILC(ip,out,op, cu |= u);
   return (cu == -1)?0:(op-out);
     #else
-  for(; ip < (in+inlen)-4; ip += 4, op += 3) { unsigned u = ctou32(ip); u = DU32(u); stou32(op, u); } 
+  for(; ip < in_-4; ip += 4, op += 3) { unsigned u = ctou32(ip); u = DU32(u); stou32(op, u); } 
   DXTAIL(ip,out,op)
   return op - out;
     #endif		
@@ -114,9 +114,9 @@ static ALWAYS_INLINE size_t _tb64xd(const unsigned char *in, size_t inlen, unsig
 #if defined(__SSSE3__)
 #include <tmmintrin.h>
 #define BITPACK128V8_6(v, cpv) {\
-  const __m128i merge_ab_and_bc = _mm_maddubs_epi16(v,            _mm_set1_epi32(0x01400140));  /*dec_reshuffle: https://arxiv.org/abs/1704.00605 P.17*/\
-                              v = _mm_madd_epi16(merge_ab_and_bc, _mm_set1_epi32(0x00011000));\
-                              v = _mm_shuffle_epi8(v, cpv);\
+  const __m128i merge_ab_bc = _mm_maddubs_epi16(v,            _mm_set1_epi32(0x01400140));  /*dec_reshuffle: https://arxiv.org/abs/1704.00605 P.17*/\
+                          v = _mm_madd_epi16(merge_ab_bc, _mm_set1_epi32(0x00011000));\
+                          v = _mm_shuffle_epi8(v, cpv);\
 }
 
 #define BITMAP128V8_6(iv, shifted, delta_asso, delta_values, ov) { /*map 8-bits ascii to 6-bits binary*/\

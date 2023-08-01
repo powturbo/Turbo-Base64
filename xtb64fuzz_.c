7c9ccc12ab2fb27aaf47a6706b94b5af1079b6cf
x <powturbo@gmail.com>
2023-08-01 17:41:20 +0200

    .

--- xtb64fuzz_.c
+++ xtb64fuzz_.c
@@ -1,10 +1,10 @@
-	#ifdef _BASE64 // ok
-      case 21: if(cpuini(0)>=33) { size_t outlen; base64_encode((const char*)in, n, (char*)out, &outlen, BASE64_FORCE_SSSE3); base64_decode((const char*)out, outlen, (char*)cpy, &outlen, BASE64_FORCE_SSSE3); } break;
-      case 22: if(cpuini(0)>=41) { size_t outlen; base64_encode((const char*)in, n, (char*)out, &outlen, BASE64_FORCE_SSE41); base64_decode((const char*)out, outlen, (char*)cpy, &outlen, BASE64_FORCE_SSE41); } break; 
-      case 23: if(cpuini(0)>=50) { size_t outlen; base64_encode((const char*)in, n, (char*)out, &outlen, BASE64_FORCE_AVX);   base64_decode((const char*)out, outlen, (char*)cpy, &outlen, BASE64_FORCE_AVX); }  break;
+	    #ifdef _BASE64 // ok
+      case 21: if(cpuini(0)>=0x33) { size_t outlen; base64_encode((const char*)in, n, (char*)out, &outlen, BASE64_FORCE_SSSE3); base64_decode((const char*)out, outlen, (char*)cpy, &outlen, BASE64_FORCE_SSSE3); } break;
+      case 22: if(cpuini(0)>=0x41) { size_t outlen; base64_encode((const char*)in, n, (char*)out, &outlen, BASE64_FORCE_SSE41); base64_decode((const char*)out, outlen, (char*)cpy, &outlen, BASE64_FORCE_SSE41); } break; 
+      case 23: if(cpuini(0)>=0x50) { size_t outlen; base64_encode((const char*)in, n, (char*)out, &outlen, BASE64_FORCE_AVX);   base64_decode((const char*)out, outlen, (char*)cpy, &outlen, BASE64_FORCE_AVX); }  break;
       case 24: if(cpuini(0)>=0x60) { size_t outlen; base64_encode((const char*)in, n, (char*)out, &outlen, BASE64_FORCE_AVX2); base64_decode((const char*)out, l, (char*)cpy, &outlen, BASE64_FORCE_AVX2);} break;
-	#endif
-	#ifdef _FASTBASE
+	    #endif
+	    #ifdef _FASTBASE // Not Ok
       case 33: if(cpuini(0) >= 60) { l=fast_avx2_base64_encode((char*)out, (const char*)in, n); fast_avx2_base64_decode((char*)cpy,(const char*)out,l); } break;
         #endif
         #ifdef _CRZY
